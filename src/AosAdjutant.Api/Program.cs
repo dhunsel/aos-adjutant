c775a736fb6a313a8d6c9ad73dab169775225db0
Dennis Van hunsel <dennis.vanhunsel@outlook.com>
2026-03-28 23:23:32 +0100

    setup serilog

--- src/AosAdjutant.Api/Program.cs
+++ src/AosAdjutant.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text.Json.Serialization;
 using AosAdjutant.Api.Database;
 using AosAdjutant.Api.Features.Abilities;
@@ -7,43 +8,84 @@ using AosAdjutant.Api.Features.Factions;
 using AosAdjutant.Api.Features.Units;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+// Bootstrap logger to log errors during startup
+// Replaced at a later step with a fully-featured logger
+Log.Logger = new LoggerConfiguration().WriteTo
+    .Console(formatProvider: CultureInfo.InvariantCulture)
+    .CreateBootstrapLogger();
 
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(opts =>
-        opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false))
+Log.Information("Starting the application");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, lc) => lc.ReadFrom
+        .Configuration(builder.Configuration)
+        .ReadFrom
+        .Services(services)
+        .Enrich
+        .FromLogContext()
     );
 
-builder.Services.AddScoped<FactionService, FactionService>();
-builder.Services.AddScoped<BattleFormationService, BattleFormationService>();
-builder.Services.AddScoped<UnitService, UnitService>();
-builder.Services.AddScoped<AttackProfileService, AttackProfileService>();
-builder.Services.AddScoped<AbilityService, AbilityService>();
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(opts =>
+            opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false))
+        );
 
-builder.Services.AddProblemDetails();
+    builder.Services.AddScoped<FactionService, FactionService>();
+    builder.Services.AddScoped<BattleFormationService, BattleFormationService>();
+    builder.Services.AddScoped<UnitService, UnitService>();
+    builder.Services.AddScoped<AttackProfileService, AttackProfileService>();
+    builder.Services.AddScoped<AbilityService, AbilityService>();
 
-builder.Services.AddHttpLogging(opts => { });
+    builder.Services.AddProblemDetails();
 
-builder.Services.AddOpenApi();
+    builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-    opt.UseNpgsql(builder.Configuration["AosAdjutant:DbContextConnectionString"])
-);
+    builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+        opt.UseNpgsql(builder.Configuration["AosAdjutant:DbContextConnectionString"])
+    );
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.UseHttpLogging();
+    app.UseSerilogRequestLogging(opts =>
+        {
+            opts.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            {
+                diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+                diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+            };
+        }
+    );
 
-app.UseExceptionHandler();
+    app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.MapControllers();
+
+    await app.RunAsync();
+
+    Log.Information("Application shutdown successful");
+    return 0;
 }
-
-app.MapControllers();
-
-await app.RunAsync();
+#pragma warning disable CA1031 // Ignore here because we catch exception just to log it before shutdown
+catch (Exception ex)
+#pragma warning restore CA1031
+{
+    Log.Fatal(ex, "Unhandled exception during startup");
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
