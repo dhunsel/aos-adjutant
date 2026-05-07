3f60017d6f1b12db27598dc3cb3e1d2ad3bbb117
Dennis Van hunsel <dennis.vanhunsel@outlook.com>
2026-05-07 18:27:43 +0200

    add faction list filter

--- frontend/src/features/dashboard/factions/pages/faction-list-page.tsx
+++ frontend/src/features/dashboard/factions/pages/faction-list-page.tsx
@@ -1,10 +1,13 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useFactions } from "../faction.queries";
-import type { FactionQuery, GrandAlliance } from "@/types/api.types";
+import type { GrandAlliance } from "@/types/api.types";
 import { Badge } from "@/components/ui/badge";
-import { useState, type ComponentProps } from "react";
+import { type ComponentProps } from "react";
 import { cn } from "@/lib/utils";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useSearchParams } from "react-router";
 import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 const GrandAllianceBadge = ({
   grandAlliance,
@@ -39,41 +42,54 @@ const GrandAllianceBadge = ({
 };
 
 export function FactionListPage() {
-  const [filter, setFilter] = useState<FactionQuery>();
-  const factions = useFactions(filter);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const grandAlliance = searchParams.get("grandAlliance") as GrandAlliance;
+  const factions = useFactions({ GrandAlliance: grandAlliance });
 
-  if (factions.isLoading)
-    return (
-      <div className="pt-10">
-        <Spinner className="mx-auto" />
-      </div>
-    );
+  const onFilterClick = (value: string[]) => {
+    const newVal = value[0] ? { grandAlliance: value[0] } : undefined;
+    setSearchParams(newVal);
+  };
 
   return (
-    <div className="flex-col">
-      <div className="flex items-center gap-2 bg-card pb-2">
-        <span>Filter</span>
-        <GrandAllianceBadge
-          onClick={() => {
-            setFilter({ GrandAlliance: "Order" });
-          }}
-          grandAlliance="Order"
-        />
-        <GrandAllianceBadge grandAlliance="Chaos" />
-        <GrandAllianceBadge grandAlliance="Death" />
-        <GrandAllianceBadge grandAlliance="Destruction" />
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between">
+        <ToggleGroup value={[grandAlliance]} onValueChange={onFilterClick}>
+          <ToggleGroupItem variant="outline" value="Order">
+            Order
+          </ToggleGroupItem>
+          <ToggleGroupItem variant="outline" value="Chaos">
+            Chaos
+          </ToggleGroupItem>
+          <ToggleGroupItem variant="outline" value="Death">
+            Death
+          </ToggleGroupItem>
+          <ToggleGroupItem variant="outline" value="Destruction">
+            Destruction
+          </ToggleGroupItem>
+        </ToggleGroup>
+        <Button variant="outline">
+          <Plus />
+          Add Faction
+        </Button>
       </div>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-3">
-        {factions.data?.items.map((f) => (
-          <div
-            key={f.factionId}
-            className="flex flex-col gap-2 rounded-lg border border-border bg-card p-2 text-nowrap text-card-foreground"
-          >
-            <span className="font-heading text-xl">{f.name}</span>
-            <GrandAllianceBadge grandAlliance={f.grandAlliance} />
-          </div>
-        ))}
-      </div>
+      {factions.isLoading ? (
+        <div className="pt-10">
+          <Spinner className="mx-auto" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-3">
+          {factions.data?.items.map((f) => (
+            <div
+              key={f.factionId}
+              className="flex flex-col gap-2 rounded-lg border border-border bg-card p-2 text-nowrap text-card-foreground"
+            >
+              <span className="font-heading text-xl">{f.name}</span>
+              <GrandAllianceBadge grandAlliance={f.grandAlliance} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
