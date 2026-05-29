1788cbfae3680557ee3aaff1be16c813ba5cea4d
Dennis Van hunsel <dennis.vanhunsel@outlook.com>
2026-05-29 21:52:40 +0200

    setup forward headers + env cleanup

--- infra/postgres/init/01-create-roles.sh
+++ infra/postgres/init/01-create-roles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-psql -v ON_ERROR_STOP=1\
-     --username "$POSTGRES_USER"
+psql -v ON_ERROR_STOP=1 \
+     --username "$POSTGRES_USER" \
      --dbname "$POSTGRES_DB" <<- EOSQL
 
     CREATE ROLE migrator WITH LOGIN PASSWORD '${MIGRATOR_PASSWORD}';
